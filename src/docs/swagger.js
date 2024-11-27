b8cb067d44a8162f935c5af5c8372ed3c78e0de9
HengkerKucing <luqmanzzx@gmail.com>
2024-11-27 14:12:40 +0700

    fix: bug deployment v2

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -9,7 +9,7 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: process.env.API_BASE_URL || 'http://localhost:3000',
+      url: 'http://localhost:3000',
     },
   ],
   components: {
