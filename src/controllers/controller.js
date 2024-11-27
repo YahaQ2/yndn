939dc90d012ae0b8a66645dee28a1b0c96df9d4e
HengkerKucing <luqmanzzx@gmail.com>
2024-11-27 09:57:32 +0700

    feat & refactor: add param to search menfess, add get menfess by id, & refactor code

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -7,11 +7,22 @@ class Controller {
      * @swagger
      * /v1/api/menfess:
      *   get:
-     *     summary: Mendapatkan daftar semua menfess
+     *     summary: Mendapatkan daftar menfess dengan filter berdasarkan query parameters
      *     tags: [Menfess]
+     *     parameters:
+     *       - in: query
+     *         name: sender
+     *         schema:
+     *           type: string
+     *         description: Nama pengirim yang ingin dicari
+     *       - in: query
+     *         name: recipient
+     *         schema:
+     *           type: string
+     *         description: Nama penerima yang ingin dicari
      *     responses:
      *       200:
-     *         description: Daftar semua menfess berhasil diambil
+     *         description: Daftar menfess berhasil diambil
      *         content:
      *           application/json:
      *             schema:
@@ -21,15 +32,42 @@ class Controller {
      *       500:
      *         description: Terjadi kesalahan pada server
      */
-    static async index(req, res) {
+    static async getMenfess(req, res) {
         try {
-            const menfesses = await prisma.menfess.findMany()
-            return res.status(200).json(response(true, true, null, menfesses))
+            const { sender, recipient } = req.query;
+
+            const filters = {};
+            if (sender) {
+                filters.sender = {
+                    contains: sender.toLowerCase(),
+                };
+            }
+            if (recipient) {
+                filters.recipient = {
+                    contains: recipient.toLowerCase(),
+                };
+            }
+    
+            const menfesses = await prisma.menfess.findMany({
+                where: filters,
+            });
+    
+            if (menfesses.length === 0) {
+                return res.status(404).json(response(false, false, "Menfess tidak ditemukan", null));
+            }
+    
+            return res.status(200).json(response(true, true, null, menfesses));
         } catch (error) {
-            console.error(error)
-            return res.status(500).json(response(false, false, "Internal Server Error", null))
+            console.error(error);
+
+            if (error instanceof PrismaClientValidationError) {
+                return res.status(400).json(response(false, false, "Error dalam validasi Prisma", null));
+            }
+            
+            return res.status(500).json(response(false, false, "Internal Server Error", null));
         }
     }
+    
 
     /**
      * @swagger
@@ -55,7 +93,7 @@ class Controller {
      *       500:
      *         description: Terjadi kesalahan pada server
      */
-    static async store(req, res) {
+    static async createMenfess(req, res) {
         try {
             const { sender, message, song, recipient } = req.body
 
@@ -73,6 +111,47 @@ class Controller {
             })
 
             return res.status(201).json(response(true, true, "Success create menfess", newMenfess))
+        } catch (error) {
+            console.error(error)
+            return res.status(500).json(response(false, false, "Internal Server Error", null))
+        }
+    }
+
+
+    /**
+     * @swagger
+     * /v1/api/menfess/{id}:
+     *   get:
+     *     summary: Menemukan menfess berdasarkan ID
+     *     tags: [Menfess]
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         schema:
+     *           type: integer
+     *         required: true
+     *         description: ID menfess yang akan diperbarui
+     *     responses:
+     *       200:
+     *         description: Menfess berhasil diperbarui
+     *         content:
+     *           application/json:
+     *             schema:
+     *               $ref: '#/components/schemas/Menfess'
+     *       404:
+     *         description: Menfess tidak ditemukan
+     *       500:
+     *         description: Terjadi kesalahan pada server
+     */
+    static async getMenfessById(req, res) {
+        try {
+            const id = req.params.id
+
+            const menfessById = await prisma.menfess.findUnique({
+                where: { id: parseInt(id) },
+            })
+
+            return res.status(200).json(response(true, true, "Success update menfess", menfessById))
         } catch (error) {
             console.error(error)
             return res.status(500).json(response(false, false, "Internal Server Error", null))
@@ -110,7 +189,7 @@ class Controller {
      *       500:
      *         description: Terjadi kesalahan pada server
      */
-    static async edit(req, res) {
+    static async editMenfess(req, res) {
         try {
             const id = req.params.id
             const { sender, message, song, recipient } = req.body
@@ -154,7 +233,7 @@ class Controller {
      *       500:
      *         description: Terjadi kesalahan pada server
      */
-    static async delete(req, res) {
+    static async deleteMenfess(req, res) {
         try {
             const id = req.params.id
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,9 +3,10 @@ const router = express.Router()
 const MenfessController = require('../controllers/controller')
 
 // Routes Menfess
-router.get('/menfess', MenfessController.index)
-router.post('/menfess', MenfessController.store)
-router.put('/menfess/:id', MenfessController.edit)
-router.delete('/menfess/:id', MenfessController.delete)
+router.get('/menfess', MenfessController.getMenfess)
+router.post('/menfess', MenfessController.createMenfess)
+router.put('/menfess/:id', MenfessController.editMenfess)
+router.get('/menfess/:id', MenfessController.getMenfessById)
+router.delete('/menfess/:id', MenfessController.deleteMenfess)
 
 module.exports = router
