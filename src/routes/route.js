fa455d46359e4b71135a1682cd1d64d47dcc6fec
HengkerKucing <140718591+HengkerKucing@users.noreply.github.com>
2024-11-30 22:38:08 +0700

    hidupin route

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,14 +4,14 @@ const Controller = require('../controllers/controller')
 const MenfessController  = require('../controllers/spotify-controller')
 
 // Routes Menfess
-router.get('/menfess', Controller.getMenfess)
+// router.get('/menfess', Controller.getMenfess)
 // router.post('/menfess', Controller.createMenfess)
 // router.put('/menfess/:id', MenfessController.editMenfess)
-router.get('/menfess/:id', Controller.getMenfessById)
+// router.get('/menfess/:id', Controller.getMenfessById)
 // router.delete('/menfess/:id', MenfessController.deleteMenfess)
 
 // Route Menfess v2
-// router.post('/menfess-spotify', MenfessController.createMenfessWithSpotify)
+router.post('/menfess-spotify', MenfessController.createMenfessWithSpotify)
 router.get('/menfess-spotify-search', MenfessController.getMenfessSpotify)
 router.get('/menfess-spotify-search/:id', MenfessController.getMenfessSpotifyById)
 router.get('/search-spotify-song', MenfessController.searchSpotifySong)
