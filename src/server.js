2af148e4f4d47ad1fcd5d5cc32a47c887d7282e8
Hacoree <feryputrafer@gmail.com>
2024-12-04 15:34:58 +0700

    update src/server.js

--- src/server.js
+++ src/server.js
@@ -27,5 +27,5 @@ app.get('/swagger.json', (req, res) => {
 });
 app.use(express.static(path.join(__dirname, '../public')));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
