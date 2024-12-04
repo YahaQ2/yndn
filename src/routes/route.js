bae6bb09479e41b1d08564b2790e944d6ce84a69
Hacoree <feryputrafer@gmail.com>
2024-12-05 00:59:07 +0700

    update src/routes/route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const Controller = require('../controllers/controller')
 const MenfessController  = require('../controllers/spotify-controller')
-const verifyCaptcha = require('../middleware/verifyCaptcha');
 
 // Routes Menfess
-// router.get('/menfess', Controller.getMenfess)
-// router.post('/menfess', Controller.createMenfess)
+router.get('/menfess', Controller.getMenfess)
+router.post('/menfess', Controller.createMenfess)
 // router.put('/menfess/:id', MenfessController.editMenfess)
-// router.get('/menfess/:id', Controller.getMenfessById)
+router.get('/menfess/:id', Controller.getMenfessById)
 // router.delete('/menfess/:id', MenfessController.deleteMenfess)
 
 // Route Menfess v2
-// router.post('/menfess-spotify', verifyCaptcha, MenfessController.createMenfessWithSpotify);
-router.get('/menfess-spotify-search', MenfessController.getMenfessSpotify);
-router.get('/menfess-spotify-search/:id', MenfessController.getMenfessSpotifyById);
-router.get('/search-spotify-song', MenfessController.searchSpotifySong);
-
+router.post('/menfess-spotify', MenfessController.createMenfessWithSpotify)
+router.get('/menfess-spotify-search', MenfessController.getMenfessSpotify)
+router.get('/menfess-spotify-search/:id', MenfessController.getMenfessSpotifyById)
+router.get('/search-spotify-song', MenfessController.searchSpotifySong)
 
 module.exports = router
