c265c580fc1fbadfb1d97fb33e65d22c442c3e71
Hacoree <feryputrafer@gmail.com>
2024-12-05 17:27:35 +0700

    update src/docs/swagger.js

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -1,63 +1,66 @@
-const swaggerJsDoc = require('swagger-jsdoc');
+const path = require('path');
+const swaggerJSDoc = require('swagger-jsdoc');
 
-const swaggerOptions = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "API Documentation",
-      version: "1.0.0",
-      description: "Dokumentasi API dengan Swagger",
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'API Documentation',
+    version: '1.0.0',
+    description: 'Dokumentasi API dengan Swagger',
+  },
+  servers: [
+    {
+      url: process.env.NODE_ENV === 'production' 
+        ? 'https://unand.vercel.app' 
+        : 'http://localhost:3000',
     },
-    servers: [
-      {
-        url: "https://unand.vercel.app",
-      },
-    ],
-    components: {
-      schemas: {
-        Menfess: {
-          type: "object",
-          properties: {
-            id: {
-              type: "integer",
-              description: "ID menfess yang unik",
-            },
-            sender: {
-              type: "string",
-              description: "Pengirim menfess",
-            },
-            message: {
-              type: "string",
-              description: "Pesan dari menfess",
-            },
-            song: {
-              type: "string",
-              description: "Lagu terkait dengan menfess (opsional)",
-            },
-            recipient: {
-              type: "string",
-              description: "Penerima menfess",
-            },
-            createdAt: {
-              type: "timestamp",
-              format: "date-time",
-              description: "Tanggal dan waktu saat menfess dibuat",
-            },
-            updatedAt: {
-              type: "timestamp",
-              format: "date-time",
-              description: "Tanggal dan waktu saat menfess terakhir diperbarui",
-            },
+  ],
+  components: {
+    schemas: {
+      Menfess: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'integer',
+            description: 'ID menfess yang unik',
+          },
+          sender: {
+            type: 'string',
+            description: 'Pengirim menfess',
+          },
+          message: {
+            type: 'string',
+            description: 'Pesan dari menfess',
+          },
+          song: {
+            type: 'string',
+            description: 'Lagu terkait dengan menfess (opsional)',
+          },
+          recipient: {
+            type: 'string',
+            description: 'Penerima menfess',
+          },
+          createdAt: {
+            type: 'string',
+            format: 'date-time',
+            description: 'Tanggal dan waktu saat menfess dibuat',
+          },
+          updatedAt: {
+            type: 'string',
+            format: 'date-time',
+            description: 'Tanggal dan waktu saat menfess terakhir diperbarui',
           },
         },
       },
     },
-    paths: {},
-    tags: [],
   },
-  apis: [], // Tambahkan jalur file di sini jika Anda menggunakan anotasi untuk endpoint API
 };
 
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
+const options = {
+  swaggerDefinition,
+  apis: [path.join(__dirname, '../controllers/*.js')],
+};
 
-module.exports = swaggerDocs;
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
