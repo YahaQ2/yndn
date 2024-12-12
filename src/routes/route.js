7738357b0291e62da6a7b5807a8e480b56ee917d
Hacoree <feryputrafer@gmail.com>
2024-12-13 00:58:08 +0700

    update src/routes/route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const Controller = require('../controllers/controller')
-const MenfessController  = require('../controllers/spotify-controller')
+const express = require('express');
+const router = express.Router();
+const menfessController = require('../controllers/menfessController');
 
-// Routes Menfess
-router.get('/menfess', Controller.getMenfess)
-router.post('/menfess', Controller.createMenfess)
-// router.put('/menfess/:id', MenfessController.editMenfess)
-router.get('/menfess/:id', Controller.getMenfessById)
-// router.delete('/menfess/:id', MenfessController.deleteMenfess)
+router.get('/menfess', menfessController.getMenfesses);
+router.post('/menfess', menfessController.createMenfess);
+router.get('/menfess/:id', menfessController.getMenfessById);
+router.put('/menfess/:id', menfessController.updateMenfess);
+router.delete('/menfess/:id', menfessController.deleteMenfess);
 
-// Route Menfess v2
-router.post('/menfess-spotify', MenfessController.createMenfessWithSpotify)
-router.get('/menfess-spotify-search', MenfessController.getMenfessSpotify)
-router.get('/menfess-spotify-search/:id', MenfessController.getMenfessSpotifyById)
-router.get('/search-spotify-song', MenfessController.searchSpotifySong)
-
-module.exports = router
+module.exports = router;
