d6a7ed7f455b7dea9c732a14ee3f0eb245b5ff35
Hacoree <feryputrafer@gmail.com>
2024-12-13 00:47:13 +0700

    update src/server.js

--- src/server.js
+++ src/server.js
@@ -1,31 +1,18 @@
 require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const route = require('./routes/route');
+const routes = require('./routes');
+
 const app = express();
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./docs/swagger');
-const path = require('path');
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello from Server yunand Menfess');
+  res.send('Hello from Yunand Menfess Server');
 });
 
-// Route untuk API
-app.use('/v1/api', route);
-
-// Route Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.get('/swagger.json', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(swaggerSpec);
-});
-app.use(express.static(path.join(__dirname, '../public')));
+app.use('/api', routes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
