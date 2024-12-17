3c23511481dd6c3c3a58c79e4e3e6cdd9343ad0c
Hacoree <feryputrafer@gmail.com>
2024-12-18 00:51:37 +0700

    update src/server.js

--- src/server.js
+++ src/server.js
@@ -22,8 +22,4 @@ app.use(express.static(path.join(__dirname, '../public')));
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
-
-
-
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
