03c84e429af257d35ca2e131ccc066238c5a38d2
YahaQ2 <vivopopupcera@gmail.com>
2024-12-20 16:02:22 +0700

    update src/server.js

--- src/server.js
+++ src/server.js
@@ -29,3 +29,13 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+
+
+
+
+const commentsRoutes = require("./routes/comments");
+
+// Routes
+app.use("/api/comments", commentsRoutes);
+
