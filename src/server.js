e61f7f5ba5d75b2cbe649076dda77864690e4cb2
YahaQ2 <vivopopupcera@gmail.com>
2024-12-22 17:14:36 +0700

    update src/server.js

--- src/server.js
+++ src/server.js
@@ -15,7 +15,9 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.get('/', (req, res) => {
+  res.send('Hello from Server Menfess');
+});
 // Swagger options
 const swaggerOptions = {
   swaggerDefinition: {
