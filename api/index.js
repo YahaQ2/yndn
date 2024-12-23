a4fe974266fd5daea66eed2787b8fb8ce54b4c70
YahaQ2 <vivopopupcera@gmail.com>
2024-12-23 12:10:46 +0700

    update api/index.js

--- api/index.js
+++ api/index.js
@@ -1,21 +1,31 @@
-import express from 'express';
-import cors from 'cors';
-import { swaggerDocument } from './swagger.js';
-import swaggerUi from 'swagger-ui-express';
-import router from './route.js';
+require('dotenv').config();
 
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const route = require('./routes/route');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./docs/swagger');
+const path = require('path');
+
 app.use(cors());
-app.use(express.json());
-
-// Swagger documentation
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// Routes
-app.use('/api', router);
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('API is running!');
+  res.send('Hello from Server Menfess');
 });
 
-export default app;
+// Route untuk API
+app.use('/v1/api', route);
+
+// Route Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get('/swagger.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+app.use(express.static(path.join(__dirname, '../public')));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
