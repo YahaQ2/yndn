b8f4b3ce19bf9f89ef73529ecf2629e9196c074b
YahaQ2 <vivopopupcera@gmail.com>
2024-12-23 10:20:24 +0700

    update src/server.js

--- src/server.js
+++ src/server.js
@@ -1,30 +1,31 @@
-import express from 'express'
-import cors from 'cors'
-import swaggerUi from 'swagger-ui-express'
-import { swaggerDocument } from './swagger.js'
-import commentsRouter from './routes/comments.routes.js'
+require('dotenv').config();
 
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const route = require('./routes/route');
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./docs/swagger');
+const path = require('path');
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-// Serve Swagger documentation
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-app.get('/api-docs/swagger.json', (req, res) => {
-  res.json(swaggerDocument)
-})
-
-// Serve static Swagger HTML
 app.get('/', (req, res) => {
-  res.sendFile('swagger.html', { root: '.' })
-})
+  res.send('Hello from Server Menfess');
+});
 
-// API routes
-app.use('/api', commentsRouter)
+// Route untuk API
+app.use('/v1/api', route);
 
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+// Route Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get('/swagger.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+app.use(express.static(path.join(__dirname, '../public')));
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
