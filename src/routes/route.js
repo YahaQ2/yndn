65debaebfd051412fb5b93ed1cb0ec6cd3627c5c
YahaQ2 <vivopopupcera@gmail.com>
2024-12-27 17:18:20 +0700

    update src/routes/route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,21 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const Controller = require('../controllers/controller')
-const MenfessController  = require('../controllers/commentController')
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController');
 
-// Routes Menfess
-router.get('/comments', commentController.getComments)
-router.post('/comments', Controller.createComments
-)
-// router.put('/menfess/:id', MenfessController.editMenfess)
-router.get('/comments/:id', Controller.getCommentsById)
-// router.delete('/menfess/:id', MenfessController.deleteMenfess)
+// Comment Routes
+router.get('/comments', commentController.getComments);
+router.post('/comments', commentController.createComment);
+router.get('/comments/:id', commentController.getCommentById);
+router.delete('/comments/:id', commentController.deleteComment);
 
-
-
-module.exports = router
-
-
-
-
-
+module.exports = router;
